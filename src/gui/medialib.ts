cf4daec776dd60bcab6d16816c879fb4d1ccf696
peter_todd <ptr.tdd@gmail.com>
2020-12-28 08:42:25 +0000

    heed mobx warning

--- src/gui/medialib.ts
+++ src/gui/medialib.ts
@@ -8,12 +8,15 @@ class MediaLibrary {
 
     constructor() {
         makeObservable(this, {
+            //somewhat prefer @decorator style, but it's warned against currently
+            //(probably still going to go ahead with it elsewhere)
             mainAssetPath: observable,
             //really, this should be 'computed' as an async result of changing mainAssetPath
             //but this is not a simple case for mobx... 'computed-async-mobx' requires older mobx...
             //hmmm. maybe I'm making life hard for myself.
             availableVideos: observable,
-            setMainAssetPath: action
+            setMainAssetPath: action,
+            setAvailableVideos: action
         });
         API.requestFileConfigPrefs().then(c => {
             this.setMainAssetPath(c.mainAssetPath!);
@@ -24,12 +27,18 @@ class MediaLibrary {
         this.mainAssetPath = newPath;
         API.requestSetMainAssetPath(newPath).then(async ok => {
             if (ok) {
-                this.availableVideos = await API.requestVideoList();
+                //nb, mobx complained about setting not from an action, presumably because of async
+                //so calling another action rather than setting availableVideos directly.
+                this.setAvailableVideos(await API.requestVideoList());
             } else {
                 console.error(`failed to set asset path ${newPath}, reverting to ${oldPath}`);
+                //mobx might complain about this async change:
                 this.mainAssetPath = oldPath;
             }
         });
+    }
+    setAvailableVideos(newVids: string[]) {
+        this.availableVideos = newVids;
     }
 }
 
