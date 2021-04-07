4bb5e75ba73b4098dbc00c4e2770c7bee307922c
Peter Todd <ptr.tdd@gmail.com>
2021-04-07 08:45:29 +0100

    don't use body-parser

--- src/server/server_comms.ts
+++ src/server/server_comms.ts
@@ -6,7 +6,6 @@
 /// websocket comms for control changes...
 
 import express from 'express'
-import bodyParser from 'body-parser'
 import * as consts from '@common/constants'
 import * as screen_server from './screen_config';
 import initFileConfig, * as file_config  from './assets/file_config'
@@ -23,17 +22,9 @@ export const localExternalIP = (() => ([] as any[]).concat(...Object.values(netw
 consts.setAddr(localExternalIP + ':' + consts.host_port); //ugh
 
 export const expApp = express();
-//https://stackoverflow.com/questions/52684372/fetch-post-request-to-express-js-generates-empty-body
-//trying to figure out whether express.json() is enough, whether we need body-parser
-//or why we shouldn't just have a JSON string and parse it ourselves.
-//express depends on body-parser already, so it's not adding to node_modules.
-//but it does seem to be adding to bundle size, largely because of iconv
-//expApp.use(express.json());
-expApp.use(bodyParser.urlencoded({extended: false}));//...
-expApp.use(bodyParser.json());
-//https://stackoverflow.com/questions/12497358/handling-text-plain-in-express-via-connect/12497793#12497793
-//(second, more recent answer)
-expApp.use(bodyParser.text({type: 'text/*'}));
+expApp.use(express.urlencoded({extended: false}));//...
+expApp.use(express.json());
+expApp.use(express.text({type: 'text/*'}));
 
 
 file_config.addRestAPI(expApp);
