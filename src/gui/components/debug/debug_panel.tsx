b26047a04a2359fe056477444634c30b32e41016
Peter Todd <ptr.tdd@gmail.com>
2021-04-07 08:50:10 +0100

    add qrcode for connecting remote gui.

--- src/gui/components/debug/debug_panel.tsx
+++ src/gui/components/debug/debug_panel.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import JsonView from './json_view'
+import QRCode from 'react-qr-code'
+import { guiURL } from '@common/constants';
 
 
 export default function DebugPanel() {
+    const url = guiURL;//`http://${location.host}/gui.html`; //ask for address of gui.html
 
     return (
         <>
         <JsonView />
+        <br />
+        <a href={url}>
+        <QRCode value={url} />
+        </a>
         </>
     )
 }
