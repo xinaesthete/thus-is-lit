b2e8993370eea14cf3b00e9f54a6bac2fb97b616
Peter Todd <ptr.tdd@gmail.com>
2021-04-20 12:06:12 +0100

    placeholder...

---
+++ src/gui/components/gyro_control.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+/**
+ * Thinking about making use of phone deviceorientation, maybe...
+ */
+export default function() {
+  //"to use this sensor, the user must grant permission to the 'gyroscope' device sensor through the Permissions API"
+  
+}
