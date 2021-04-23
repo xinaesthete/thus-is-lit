e23272e6363bcc7577ebefcc3a3fd08d81d3f5e1
Peter Todd <ptr.tdd@gmail.com>
2021-04-23 10:53:47 +0100

    display gui url as text for copy/paste

--- src/gui/components/debug/debug_panel.tsx
+++ src/gui/components/debug/debug_panel.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import JsonView from './json_view'
 import QRCode from 'react-qr-code'
 import { guiURL } from '@common/constants';
+import { Typography } from '@material-ui/core';
 
 
 export default function DebugPanel() {
     const url = guiURL;//`http://${location.host}/gui.html`; //ask for address of gui.html
-
+    //qrcode is a bit slow, maybe impacting more than thought?
     return (
         <>
         <JsonView />
+        <br />
+        <Typography>{url}</Typography>
         <br />
         <a href={url}>
         <QRCode value={url} />
