d5328eafc49890eda5f2586fafe66c69a8a89385
Peter Todd <ptr.tdd@gmail.com>
2021-04-24 13:35:48 +0100

    better use of hooks

--- src/gui/components/media_browser.tsx
+++ src/gui/components/media_browser.tsx
@@ -1,36 +1,58 @@
 import React from 'react'
 import MediaConfig from './media_config'
-import { GridList, GridListTile, GridListTileBar } from '@material-ui/core'
+import { Button, GridList, GridListTile, GridListTileBar, Typography } from '@material-ui/core'
 import { Pagination } from '@material-ui/lab'
 import { observer } from 'mobx-react'
 import mediaLib from '../medialib'
 import { useStyles } from '../theme'
 import { produce } from 'immer'
+import { useKaleidList } from '@gui/kaleid_context'
+import { VideoDescriptor } from '@common/media_model'
 
-const VideoCard = (url: string, classes: any, 
-        seekTimes: Map<string, number>, setSeekTimes: (newTimes: Map<string, number>)=>void) => {
-    const name = decodeURI(url.replace(/.*video\//, '')).split(/[\\/]/).reverse()[0];
-    //const [t, setT] = React.useState(0); //can't use hook here.
-    const setTime = (t: number) => {
-        const newTimes = produce(seekTimes, draftState=> {
-            draftState.set(url, t); //nb. needs immer 'enableMapSet()' on app start.
-        });
-        setSeekTimes(newTimes);
-    }
+interface VidAssignerProps {
+    url: string;
+}
+
+const useVidSource = (url: string) => {
+    //can't really sensibly make a VideoDescriptor from limited info...
+    //should be able to set URL without a big palavar anyway...
+}
+
+/** A set of buttons corresponding to each renderer output, to be displayed over a VideoTileInner. */
+const VidAssigner = observer((props: VidAssignerProps) => {
+    //context with list of renderers, such that we can add 'assign to renderer' buttons here.
+    //may also consider keystrokes.
+    const { renderModels } = useKaleidList();
+    const classes = useStyles();
     return (
-        <GridListTile key={url} cols={1} className={classes.videoTile} >
-            <video src={url} className={classes.videoPreview} style={{width: '100%'}}
+        <>
+        {renderModels.map((m, i) => {
+            return <p key={i}>{m.model.id}</p>
+        })}
+        </>
+    )
+});
+
+const VideoTileInner = ({...props}) => {
+    const { url } = props;
+    const name = React.useMemo(()=>decodeURI(url.replace(/.*video\//, '')).split(/[\\/]/).reverse()[0], [url]);
+    const classes = useStyles();
+    const [t, setTime] = React.useState(0);
+    
+    return (
+        <>
+            <video src={url} className={classes.videoPreview}
                 onMouseMove={(e) => {
                     const target = e.target as HTMLVideoElement;
                     const t = target.duration * e.nativeEvent.offsetX / target.clientWidth;
                     if (Number.isNaN(t)) return;
                     target.currentTime = t;
-                    //setT(t);
                     setTime(t);
                 }}
             />
-            <GridListTileBar title={name} subtitle={seekTimes.get(url)}/>
-        </GridListTile>
+            {/* <VidAssigner url={url} /> */}
+            <GridListTileBar title={name} subtitle={t}/>
+        </>
     )
 }
 
@@ -46,14 +68,17 @@ export default observer(function MediaBrowser() {
     const endIndex = Math.min(startIndex+maxItems, availableVideos.length);
     const displayedVideos = availableVideos.slice(startIndex, endIndex);
     
-    //lifted from VideoCard because of hooks
-    const [seekTimes, setSeekTimes] = React.useState(new Map<string, number>());
-
     return (
         <>
             <MediaConfig />
             <GridList cellHeight='auto' cols={4} className={classes.videoGridList} >
-                {displayedVideos.map((v) => VideoCard(v, classes, seekTimes, setSeekTimes))}
+                {displayedVideos.map((url) => {
+                    return (
+                        <GridListTile cols={1} className={classes.videoTile}>
+                            <VideoTileInner url={url} />
+                        </GridListTile>
+                    )
+                })}
             </GridList>
             <Pagination count={pageCount} page={page} onChange={(e, p) => setPage(p)} />
         </>
