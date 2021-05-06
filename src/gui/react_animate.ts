6be65ad7f0cf07a7c9bfb645e3bb5ddbdafbd9f2
Peter Todd <ptr.tdd@gmail.com>
2021-05-06 21:53:34 +0100

    add useAnimationFrame hook

---
+++ src/gui/react_animate.ts
@@ -0,0 +1,24 @@
+import React from 'react'
+/** 
+ * copied from https://css-tricks.com/using-requestanimationframe-with-react-hooks/
+*/
+export const useAnimationFrame = (callback: (deltaTime: number)=>void) => {
+  // Use useRef for mutable variables that we want to persist
+  // without triggering a re-render on their change
+  const requestRef = React.useRef<number>();
+  const previousTimeRef = React.useRef<number>();
+  
+  const animate = (time: number) => {
+    if (previousTimeRef.current != undefined) {
+      const deltaTime = time - previousTimeRef.current;
+      callback(deltaTime)
+    }
+    previousTimeRef.current = time;
+    requestRef.current = requestAnimationFrame(animate);
+  }
+  
+  React.useEffect(() => {
+    requestRef.current = requestAnimationFrame(animate);
+    return () => cancelAnimationFrame(requestRef.current!);
+  }, []); // Make sure the effect runs only once
+}
