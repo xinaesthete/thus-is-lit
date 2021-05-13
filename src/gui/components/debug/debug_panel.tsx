c2051d2e4b6e23c954ac1c7654a8d2b25e21bed4
Peter Todd <ptr.tdd@gmail.com>
2021-05-13 08:57:21 +0100

    add renderer link in debug panel

--- src/gui/components/debug/debug_panel.tsx
+++ src/gui/components/debug/debug_panel.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import JsonView from './json_view'
 import QRCode from 'react-qr-code'
-import { guiURL } from '@common/constants';
+import { guiURL, rendererURL } from '@common/constants';
 import { Typography } from '@material-ui/core';
 import ConnectionStatus from './connection_status';
 
+
+const QR: React.FC<{url: string, name: string}> = ({...props}) => {
+    const {url, name} = props;
+    return (
+        <>
+            <h1>Connect {name}:</h1>
+            <Typography>{url}</Typography>
+            <br />
+            <a href={url}>
+            <QRCode value={url} />
+            </a>
+        </>
+    )
+};
 
 export default function DebugPanel() {
     const url = guiURL;
@@ -14,11 +28,8 @@ export default function DebugPanel() {
         <ConnectionStatus />
         <JsonView />
         <br />
-        <Typography>{url}</Typography>
-        <br />
-        <a href={url}>
-        <QRCode value={url} />
-        </a>
+        <QR url={guiURL} name='GUI' />
+        <QR url={rendererURL} name='renderer' />
         </>
     )
 }
