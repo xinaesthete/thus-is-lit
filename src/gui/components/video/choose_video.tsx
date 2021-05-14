d5cfc8b224ca306eeefbc2eaffabb864a24f6c35
Peter Todd <ptr.tdd@gmail.com>
2021-05-14 15:16:57 +0100

    unused filter in choose_video

--- src/gui/components/video/choose_video.tsx
+++ src/gui/components/video/choose_video.tsx
@@ -5,9 +5,11 @@ import IconButton from '@material-ui/core/IconButton'
 import CasinoIcon from '@material-ui/icons/Casino'
 import MenuItem from '@material-ui/core/MenuItem'
 import mediaLib, { niceName } from '../../medialib'
-import { httpURL } from '@common/constants'
 import { useStyles } from '../../theme'
 import { VideoDescriptor } from '@common/media_model'
+// import { TextField } from '@material-ui/core'
+import Fuse from 'fuse.js'
+import { useLogGui } from '../log_gui'
 
 
 export default observer( function ChooseVideo(props: { 
@@ -15,13 +17,18 @@ export default observer( function ChooseVideo(props: {
 }) {
   
   const classes = useStyles();
+  const {log} = useLogGui();
   //TODO options for advance to next video in sequence / random after it finishes.
   const url = encodeURI(props.video.url); //would probably rather it was kept encoded.
-  
-  const availableMenu = React.useMemo(() => mediaLib.filteredVideos.map(v => (
-     <MenuItem key={v} value={v}>{niceName(v)}</MenuItem> 
-     )),
-    [mediaLib.filteredVideos]
+  const [filterText, setFilterText] = React.useState('');
+  const availableMenu = React.useMemo(() => {
+    const fuse = filterText ? new Fuse(mediaLib.filteredVideos, {ignoreLocation: true}) : null;
+    const filterList = fuse ? fuse.search(filterText).map(r => r.item) : mediaLib.filteredVideos;
+    log(filterList.length + ' vis');
+    return filterList.map(v => (
+       <MenuItem key={v} value={v}>{niceName(v)}</MenuItem> 
+     ))
+    }, [mediaLib.filteredVideos, filterText]
   );
   const random = () => {
     props.setURL(mediaLib.chooseRandom());
@@ -33,6 +40,7 @@ export default observer( function ChooseVideo(props: {
         onChange={e=>props.setURL(e.target.value as string)}>
         {availableMenu}
       </Select>
+      {/* <TextField label="filter" onChange={e=>setFilterText(e.target.value)} /> */}
     </>
   )
 });
