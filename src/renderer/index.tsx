1dad33e0508ec9d3852bd88c746248f3693a8577
Peter Todd <ptr.tdd@gmail.com>
2021-05-17 11:26:00 +0100

    rename to start with lower case

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -9,7 +9,7 @@ const params = new URLSearchParams(location.search);
 const vidUrl = params.has('vidUrl') ? params.get('vidUrl')! : 'red.mp4';
 
 const vid = new VideoState(vidUrl);
-const KRenderer = new KaleidRenderer(vid);
+const kRenderer = new KaleidRenderer(vid);
 vid.vidEl.onplay = () => {
   //console.log(`onplay`);
 }
@@ -23,24 +23,24 @@ vid.vidEl.oncanplay = () => {
 const renderer = new THREE.WebGLRenderer({antialias: true, alpha: false});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.prepend(renderer.domElement);
-KRenderer.initThree(renderer.domElement);
-commsInit(KRenderer);
+kRenderer.initThree(renderer.domElement);
+commsInit(kRenderer);
 function animate(time: number) {
   requestAnimationFrame(animate);
-  KRenderer.update(time);
-  KRenderer.render(renderer);
+  kRenderer.update(time);
+  kRenderer.render(renderer);
 }
 animate(Date.now());
 window.onresize = () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
     //camera.aspect = window.innerWidth / window.innerHeight;
-    KRenderer.resize(renderer.domElement.getBoundingClientRect());
+    kRenderer.resize(renderer.domElement.getBoundingClientRect());
 };
 registerKey('v', () => {
   vid.refreshVidElement();
 });
 registerKey('r', () => {
   console.log('recording....');
-  recordAndDownload(renderer.domElement, 15000);
+  recordAndDownload(renderer.domElement, 10000);
 });
 export default "exporting from /src/renderer/index so that src/gui/renderIndexShim has something to import...";
