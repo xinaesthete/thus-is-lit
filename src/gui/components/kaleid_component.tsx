471afd521b878231905c17f37d90232cb3a777e5
Peter Todd <ptr.tdd@gmail.com>
2021-05-18 12:30:51 +0100

    useMemo rather than useState - was expensive

--- src/gui/components/kaleid_component.tsx
+++ src/gui/components/kaleid_component.tsx
@@ -9,10 +9,9 @@ import KaleidRenderer from '../../renderer/kaleid_renderer';
 const SpecimenVersion = observer(function SpecimenVersion(props: {spec: Specimen}) {
     const classes = useStyles();
     const kaleid = useKaleid();
-    const [kRender] = React.useState(new KaleidRenderer(kaleid.vidState));
+    const kRender = React.useMemo(()=> new KaleidRenderer(kaleid.vidState, kaleid.model), []);
     kRender.setParmsFromArray([...props.spec.genes.values()]);
     console.log('react kaleid_component render');
-    // return <Threact gfx={kRender} className={classes.kaleidComponent} />
     return <Threact gfx={kRender} className={classes.kaleidComponent} />
 });
 
@@ -21,12 +20,11 @@ export default observer(function KaleidComponent(props: { spec?: Specimen }) {
     const config = useLitConfig();
     const classes = useStyles();
     const kaleid = useKaleid();
-    const [kRender] = React.useState(new KaleidRenderer(kaleid.vidState, kaleid.model));
-    //after all hooks...
+    
     if (!config.livePreviews) {
         return <div style={{width: '100px', height: '50px', backgroundColor: 'red', opacity: '0.3'}}> </div>
     }
     if (props.spec) return <SpecimenVersion spec={props.spec} />;
-    
+    const kRender = React.useMemo(()=> new KaleidRenderer(kaleid.vidState, kaleid.model), []);
     return <Threact gfx={kRender} className={classes.kaleidComponent} />
 })
