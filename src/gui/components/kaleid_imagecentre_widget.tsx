e6b72055c6401106186b4ad1d239240897d0b99d
Peter Todd <ptr.tdd@gmail.com>
2021-05-19 16:06:45 +0100

    mouse drag

--- src/gui/components/kaleid_imagecentre_widget.tsx
+++ src/gui/components/kaleid_imagecentre_widget.tsx
@@ -1,3 +1,4 @@
+import { DomAttributes } from '@common/threact/threact';
 import { vec2 } from '@common/tweakables';
 import { useKaleid } from '@gui/kaleid_context';
 import { action } from 'mobx';
@@ -7,27 +8,33 @@ import KaleidComponent from './kaleid_component';
 import { SliderProp } from './uniforms_gui';
 
 export default observer((u: SliderProp<vec2>) => {
-  const kaleid = useKaleid();
-  const [cX, setX] = React.useState(0);
-  const [cY, setY] = React.useState(0);
-  //need to get some mouse events on this thing.
   //might be useful to overlay another div that can also be used for debugging
+
+  const [mouseDown, setMouseDown] = React.useState(false);
+  const dom: DomAttributes = {
+    onClick: action((ev) => {
+      ev.preventDefault();
+      //figure out coordinate of ev (normalised -1..1) & apply it to u...
+      //looks as though currentTarget.width/height are not what I want...
+      const rect = ev.currentTarget.getBoundingClientRect();
+      const x = 2 * (ev.clientX-rect.left) / ev.currentTarget.offsetWidth - 1;
+      const y = -2 * (ev.clientY-rect.top) / ev.currentTarget.offsetHeight + 1;
+      u.onChange({x, y});  
+    })
+  };
   return (
   <>
-  <KaleidComponent previs={true} onClick={action((ev) => {
-    ev.preventDefault();
-    //figure out coordinate of ev (normalised -1..1) & apply it to u...
-    //looks as though currentTarget.width/height are not what I want...
-    const rect = ev.currentTarget.getBoundingClientRect();
-    const x = 2 * (ev.clientX-rect.left) / ev.currentTarget.offsetWidth - 1;
-    const y = -2 * (ev.clientY-rect.top) / ev.currentTarget.offsetHeight + 1;
-    console.log('click', x, y);
-    setX(ev.clientX-rect.left);
-    setY(ev.clientY-rect.top);
-    u.onChange({x, y});
-  })} />
-  {/* {u.value.x}, {u.value.y}<br />
-  {cX}, {cY} */}
+  <KaleidComponent previs={true} 
+  onClick={dom.onClick} 
+  onMouseDown={(ev)=>{
+    setMouseDown(true);
+  }} onMouseUp={(ev) => {
+    setMouseDown(false);
+  }} 
+  onMouseMove={(ev)=>{
+    if (mouseDown) dom.onClick!(ev);
+  }}
+  />
   </>
   )
 });
