fcb48d0a527b58c686dffa32a65728d811cef02b
Peter Todd <ptr.tdd@gmail.com>
2021-05-19 09:43:58 +0100

    add a 'specialWidget' flag

--- src/common/tweakables.ts
+++ src/common/tweakables.ts
@@ -31,7 +31,8 @@ export interface Tweakable<T extends Numeric> {
     delta?: number,
     wrap?: boolean,
     movement?: MovementType,
-    tags?: string[]
+    tags?: string[],
+    specialWidget?: boolean
     //scale?: (T) => T //change to be similar to MuiSlider? but that has a different idea about relation to min&max?
     //also if I want to send functions for eval across wire, I need to be careful about trust... and anyway, I'm not using this yet.
 }
