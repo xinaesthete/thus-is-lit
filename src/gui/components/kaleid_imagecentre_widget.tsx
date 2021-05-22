53a23e1c7c1e2d5fa0cb1435cfc47f797b70ae85
peter_todd <ptr.tdd@gmail.com>
2021-05-22 11:59:36 +0100

    border

--- src/gui/components/kaleid_imagecentre_widget.tsx
+++ src/gui/components/kaleid_imagecentre_widget.tsx
@@ -25,7 +25,7 @@ export default observer((u: SliderProp<vec2>) => {
       u.onChange({x, y});
     }),
     style: {
-      width: '600px', height: '340px'
+      width: '600px', height: '340px', border: '2px solid #555'
     }
   };
   return (
